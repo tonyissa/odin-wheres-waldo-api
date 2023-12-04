e4aaee3594af628ac6ead1318a76eb2089892f1c
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-12-04 00:01:08 -0500

    Update controller for POST and checking of coords

--- routes/api.js
+++ routes/api.js
@@ -4,16 +4,22 @@ var router = express.Router();
 const Score = require('../models/Score');
 const Coords = require('../models/Coords');
 
-/* GET coords */
-router.get('/get-coords', async (req, res, next) => {
-    const response = await Coords.find().exec();
-    res.json(response);
-});
-
 /* GET scores */
 router.get('/scores', async (req, res, next) => {
     const response = await Score.find().exec();
     res.json(response);
+});
+
+/* COMPARE coords */
+router.post('/compare-coords', async (req, res, next) => {
+    const response = await Coords.find().exec();
+    for (let i = 0; i < response.length; i++) {
+        if (req.body.YCoord >= response[i].YCoords[0] && req.body.YCoord <= response[i].YCoords[1] && 
+            req.body.XCoord >= response[i].XCoords[0] && req.body.XCoord <= response[i].XCoords[1]) {
+                return res.status(202).json({ name: response[i].name })
+        }
+    }
+    return res.sendStatus(200);
 });
 
 /* POST score */
